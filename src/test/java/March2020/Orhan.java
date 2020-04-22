a8eca36b2ff8709d8cc60d63dc9095fa9689670d
orhanturan3898 <orhanturan3898@gmail.com>
2020-04-22 13:37:26 -0400

    Orhan

--- src/test/java/March2020/Orhan.java
+++ src/test/java/March2020/Orhan.java
@@ -1,8 +1,7 @@
-package March2020.ZZZ_Replits;
+package March2020;
 
-public class aaa111 {
+public class Orhan {
     public static void main(String[] args) {
         System.out.println("hi");
-
     }
 }
